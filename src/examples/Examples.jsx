582cdb317b0983687ed3294b66804bf309933474
Bezrodnov Aleksey <bezrodnov.aleksey@gmail.com>
2019-07-10 22:55:59 +0400

    nothing special, added prismjs, playing with it

--- src/examples/Examples.jsx
+++ src/examples/Examples.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 
+import ScrollContext from './ScrollContext';
 import ProgressGraphExample from './ProgressGraphExample';
 
 import './Examples.scss';
 
 class Examples extends Component {
+  state = {
+    scrollPos: 0,
+  };
+
+  onScroll = e => {
+    this.setState({ scrollPos: e.target.scrollTop });
+  };
+
   render() {
     return (
-      <Scrollbars>
-        <div className="examples">
-          <ProgressGraphExample />
-        </div>
-      </Scrollbars>
+      <div className="examples">
+        <Scrollbars className="container" onScroll={this.onScroll}>
+          <ScrollContext.Provider value={this.state}>
+            <ProgressGraphExample />
+            <div style={{ height: 3000 }} />
+          </ScrollContext.Provider>
+        </Scrollbars>
+      </div>
     );
   }
 }
--- src/examples/ProgressGraphExample.jsx
+++ src/examples/ProgressGraphExample.jsx
@@ -1,71 +1,62 @@
 import React, { Component } from 'react';
-import ScrollMagic from 'scrollmagic';
+import Prism from 'prismjs';
+import '../css/prism.css';
 
-import ScrollMagicContext from '../ScrollMagicContext';
 import ProgressGraph from '../components/ProgressGraph';
+import ScrollContext from './ScrollContext';
+
+import './ProgressGraphExample.scss';
 
 class ProgressGraphExample extends Component {
-  state = { currentStep: 0 };
+  state = {
+    currentStep: 1,
+    steps: [
+      {
+        title: 'First step',
+        subtitle: 'subtitle',
+        estimate: '15 - 20 min',
+      },
+      {
+        title: 'Second step',
+        subtitle: 'subtitle',
+        estimate: '5 - 10 min',
+      },
+      {
+        title: 'Third step',
+        subtitle: 'subtitle',
+        estimate: '20 - 25 min',
+      },
+    ],
+  };
 
   componentDidMount() {
-    this.direction = 1;
-    setInterval(() => {
-      this.setState({ currentStep: this.state.currentStep + this.direction });
-      if (this.state.currentStep === 3) {
-        this.direction = -1;
-      } else if (this.state.currentStep === 0) {
-        this.direction = 1;
-      }
-    }, 1000);
-  }
-
-  componentDidUpdate() {
-    if (this.context && !this.scene) {
-      // this.scene = new ScrollMagic.Scene({ triggerElement: '#progress-graph-example' })
-      //   .setPin('#progress-graph', { pushFollowers: false })
-      //   .addTo(this.context);
-    }
+    Prism.highlightAll();
   }
 
   render() {
-    const { currentStep } = this.state;
+    const { currentStep, steps } = this.state;
     return (
-      <div
-        id="progress-graph-example"
-        className="progress-graph-example"
-        style={
-          {
-            /*height: 3000*/
-          }
-        }
-      >
-        <div id="progress-graph">
-          <ProgressGraph
-            currentStep={currentStep}
-            steps={[
-              {
-                title: 'First step',
-                subtitle: 'subtitle',
-                estimate: '15 - 20 min',
-              },
-              {
-                title: 'Second step',
-                subtitle: 'subtitle',
-                estimate: '5 - 10 min',
-              },
-              {
-                title: 'Third step',
-                subtitle: 'subtitle',
-                estimate: '20 - 25 min',
-              },
-            ]}
-          />
+      <div className="progress-graph-example">
+        <ProgressGraph currentStep={currentStep} steps={steps} />
+        <div className="mutations">
+          <pre>
+            <code className="language-javascript">{`currentStep = 0;`}</code>
+          </pre>
+          <pre>
+            <code className="language-javascript">{`currentStep = 1;`}</code>
+          </pre>
+          <pre>
+            <code className="language-javascript">{`currentStep = 2;`}</code>
+          </pre>
+          <pre>
+            <code className="language-javascript">{`currentStep = 3;`}</code>
+          </pre>
         </div>
       </div>
     );
   }
 }
 
-ProgressGraphExample.contextType = ScrollMagicContext;
+ProgressGraphExample.contextType = ScrollContext;
 
 export default ProgressGraphExample;
